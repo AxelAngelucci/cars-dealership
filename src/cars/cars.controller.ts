0cba517642ba9bbe22fcacda482f1157b696fd04
Axel Angelucci <axeljesusangelucci@gmail.com>
2023-04-14 18:36:07 -0300

    Local Crud

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -9,6 +9,8 @@ import {
   Post,
 } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCarDto } from './dtos/create-car.dto';
+import { UpdateCarDto } from './dtos/update-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -24,17 +26,20 @@ export class CarsController {
   }
 
   @Post()
-  createCar(@Body() payload) {
-    return { ok: true, payload };
+  createCar(@Body() payload: CreateCarDto) {
+    return this.carsService.create(payload);
   }
 
   @Patch(':id')
-  updateCar(@Body() payload, @Param('id', ParseUUIDPipe) id: string) {
-    return { ok: true, payload, id };
+  updateCar(
+    @Body() payload: UpdateCarDto,
+    @Param('id', ParseUUIDPipe) id: string,
+  ) {
+    return this.carsService.update(payload, id);
   }
 
   @Delete(':id')
   deleteCar(@Param('id', ParseUUIDPipe) id: string) {
-    return { message: id };
+    return this.carsService.delete(id);
   }
 }
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Car } from './interfaces/car.interface';
 import { v4 as uuid } from 'uuid';
+import { CreateCarDto } from './dtos/create-car.dto';
+import { UpdateCarDto } from './dtos/update-car.dto';
 
 @Injectable()
 export class CarsService {
@@ -16,7 +18,33 @@ export class CarsService {
 
   findOne(id: string) {
     const car = this.cars.find((car) => car.id === id);
-    if (car) return { car };
+    if (car) return car;
     throw new NotFoundException(`Car with id ${id} not found`);
+  }
+
+  create(payload: CreateCarDto) {
+    const car: Car = { id: uuid(), ...payload };
+    this.cars.push(car);
+    return car;
+  }
+
+  update(payload: UpdateCarDto, id: string) {
+    let cardDB = this.findOne(id);
+    this.cars = this.cars.map((car) => {
+      if (car.id === id) {
+        cardDB = {
+          ...cardDB,
+          ...payload,
+        };
+        return cardDB;
+      }
+      return car;
+    });
+    return cardDB;
+  }
+
+  delete(id: string) {
+    let car = this.findOne(id);
+    this.cars = this.cars.filter((car) => car.id !== id);
   }
 }
